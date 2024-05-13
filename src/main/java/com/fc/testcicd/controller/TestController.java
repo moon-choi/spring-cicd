c21ae1cdf643c98a80f43793fa06569b9a449d86
moon-choi <choimy1991@gmail.com>
2024-05-13 16:06:55 +0900

    request mapping

--- src/main/java/com/fc/testcicd/controller/TestController.java
+++ src/main/java/com/fc/testcicd/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.fc.testcicd.controller;
 
 import com.fc.testcicd.service.TestService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class TestController {
 
     private TestService testService;
